21166ad56286493cf47fb4c8885c0bd392e04592
Pajeed Reede <pajreede@gmail.com>
2017-07-20 10:57:51 +0200

    Create ProgramInfo.h

---
+++ src/ProgramInfo.h
@@ -0,0 +1,9 @@
+#ifndef __SRC_PROGRAMINFO_H__
+#define __SRC_PROGRAMINFO_H__
+
+#include <cstring>
+
+#define __VERSION__ "0.0.1"
+const string __VERSIONINFO__ = "NRHack Engine v" + __VERSION__ + ", built at " + __TIMESTAMP__ + "\n";
+
+#endif
