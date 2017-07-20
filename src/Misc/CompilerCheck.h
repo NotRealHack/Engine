fe00e9b32d22c928d775156b7bb2201e7d678139
Pajeed Reede <pajreede@gmail.com>
2017-07-20 10:51:09 +0200

    Create CompilerCheck.h

---
+++ src/Misc/CompilerCheck.h
@@ -0,0 +1,32 @@
+//This file MUST be included at least once in order to check our compilation environment!
+
+#ifdef _MSC_VER
+# ifdef _KERNEL_MODE
+#  error "What are you doing here? A kernel mode game? I don't believe you :)"
+# endif
+# ifndef _CPPUNWIND
+#  error "Exception Handling? What is that? And why do I need it? Lol"
+# endif
+# ifdef _DLL
+#  error "This won't be a dll file!"
+# endif
+# ifndef __AVX2__
+#  error "We need AVX2!"
+# endif
+# ifndef _WIN64
+#  error "This is an Win64 Program!"
+# endif
+#elif __GNUC__
+# ifdef __ELF__
+#  error "This shall not be an ELF program!"
+# endif
+# ifdef __GNUC_GNU_INLINE__
+#  warning "Handling inline functions in GNU90 standard can be painful..."
+# endif
+# ifndef __EXCEPTIONS
+#  error "Exception handling? In an program? Why? Lol"
+# endif
+# ifndef __SSP__ || __SSP_ALL__ || __SSP_STRONG__ || __SSP_EXPLICIT__
+#  error "Stack protecting? Never heard of that^^ (I WANT IT)"
+# endif
+#endif
